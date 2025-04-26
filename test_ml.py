b194dbcdc703eef334e1056ddee77545e9447f4f
WunOne <chris.r.teal@outlook.com>
2025-04-26 15:12:40 -0400

    Complete unit tests in test_ml.py; added screenshot to screenshots folder.

--- test_ml.py
+++ test_ml.py
@@ -1,11 +1,14 @@
 import pytest
 import numpy as np
+import pickle
 from sklearn.ensemble import RandomForestClassifier
-from ml.model import (train_model, compute_model_metrics)
+from ml.model import (
+    train_model,
+    compute_model_metrics,
+    save_model,
+    load_model
+)
 from train_model import X_train, y_train
-
-
-# TODO: add necessary import
 
 def testTrainAlgorithm():
     """
@@ -14,8 +17,6 @@ def testTrainAlgorithm():
     model = train_model(X_train, y_train)
     assert isinstance(model, RandomForestClassifier), f"The incorrect training algorithm was used: {type(model)}"
 
-
-# TODO: implement the second test. Change the function name and input as needed
 def testMetricValues():
     """
     # This test is designed to determine if the metrics return a value between 0 and 1.
@@ -28,15 +29,22 @@ def testMetricValues():
     assert 0 <= r <= 1, "Recall score is outside of expected value bounds"
     assert 0 <= fb <= 1, "fBeta is outside of expected value bounds"
 
-# TODO: implement the third test. Change the function name and input as needed
-def testMetricType():
+def testSaveLoad(tmp_path):
     """
-    # This test is designed to determine if the returned metrics are of the correct type, float.    """
+    # This test is designed to validate save_model and load_model function correctly.
+    """
 
-    y_true = np.array([0,1,1,0])
-    y_pred = np.array([0,1,1,0])
-    p, r, fb = compute_model_metrics(y_true, y_pred)
+    X = np.array([[1,3], [5,7], [9, 11]])
+    y = np.array([0,1,0])
 
-    assert isinstance(p, float), "Precision did not return a float"
-    assert isinstance(r, float), "Recall score did not return a float"
-    assert isinstance(fb, float), "fBeta did not return a float"
+    model = train_model(X, y)
+
+    temp = tmp_path / "rf.pkl"
+
+    save_model(model, str(temp))
+
+    loaded = load_model(str(temp))
+
+    assert isinstance(loaded, RandomForestClassifier), f"The loaded model is using the incorrect training model: {type(loaded)}"
+
+    np.testing.assert_array_equal(model.predict(X), loaded.predict(X))
